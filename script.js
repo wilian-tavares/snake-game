9c9441bf4146a65913f528fb9812468397d07905
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-07 18:39:16 -0300

    definir direções e impedir q a cobrinha suma

--- script.js
+++ script.js
@@ -22,7 +22,24 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update, false);
+
+function update(event) {
+    if (event.keycode == 37 && direction != 'right') direction = 'left';
+    if (event.keycode == 38 && direction != 'down') direction = 'up';
+    if (event.keycode == 39 && direction != 'left') direction = 'right';
+    if (event.keycode == 40 && direction != 'up') direction = 'down';
+}
+
+
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
+
+    if (snake[0].y > 15 * box && direction == 'up') snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == 'down') snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
 
@@ -45,3 +62,4 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100);
+
