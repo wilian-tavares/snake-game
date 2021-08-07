74a0ca4f9ba7fd26dc1fca6ee20bcc4b2b1daaae
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-07 19:06:20 -0300

    definir aleatoridade da frutinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -59,7 +59,14 @@ function iniciarJogo() {
     if (directin == "up") snakeY -= box;
     if (directin == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
