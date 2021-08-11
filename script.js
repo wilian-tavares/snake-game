df2065369f5151898d4e1c7f690605121e45bf26
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-11 11:35:55 -0300

    teste de touch

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Check if pointer events are supported.
+if (window.PointerEvent) {
+    // Add Pointer Event Listener
+    swipeFrontElement.addEventListener('pointerdown', this.handleGestureStart, true);
+    swipeFrontElement.addEventListener('pointermove', this.handleGestureMove, true);
+    swipeFrontElement.addEventListener('pointerup', this.handleGestureEnd, true);
+    swipeFrontElement.addEventListener('pointercancel', this.handleGestureEnd, true);
+} else {
+    // Add Touch Listener
+    swipeFrontElement.addEventListener('touchstart', this.handleGestureStart, true);
+    swipeFrontElement.addEventListener('touchmove', this.handleGestureMove, true);
+    swipeFrontElement.addEventListener('touchend', this.handleGestureEnd, true);
+    swipeFrontElement.addEventListener('touchcancel', this.handleGestureEnd, true);
+
+    // Add Mouse Listener
+    swipeFrontElement.addEventListener('mousedown', this.handleGestureStart, true);
+}
+
+
+
+
+
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); //....
 let box = 32;
