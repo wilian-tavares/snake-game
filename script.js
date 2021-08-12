4c312460ca5e7fd4cf79e64b30531a66408847d5
wilian-tavares <wilian.tavares@souunit.com.br>
2021-08-12 11:31:34 -0300

    Adicionar comandos teclas  W-A-S-D

--- script.js
+++ script.js
@@ -37,10 +37,10 @@ function drawFood() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if (event.keyCode == 37 && direction != 'right') direction = 'left';
-    if (event.keyCode == 38 && direction != 'down') direction = 'up';
-    if (event.keyCode == 39 && direction != 'left') direction = 'right';
-    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+    if (event.keyCode == 37 && direction != 'right' || event.keyCode == 65 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down' || event.keyCode == 87 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left' || event.keyCode == 68 && direction != ' left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up' || event.keyCode == 83 && direction != 'up') direction = 'down';
 }
 
 function iniciarJogo() {
@@ -113,4 +113,8 @@ function fimDeJogo() {
 }
 function retornarJogo() {
     window.location.href = 'index.html'
+}
+
+function tentarNovamente() {
+    window.location.href = 'snake-game-inicio.html'
 }
